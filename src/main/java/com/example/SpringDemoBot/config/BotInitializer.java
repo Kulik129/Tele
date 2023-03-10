c5d9588cb751887b106e6444eb32334d0148ead2
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2023-03-10 23:29:15 +0300

    Проверил работу бота

--- src/main/java/com/example/SpringDemoBot/config/BotInitializer.java
+++ src/main/java/com/example/SpringDemoBot/config/BotInitializer.java
@@ -1,6 +1,7 @@
 package com.example.SpringDemoBot.config;
 
 import com.example.SpringDemoBot.service.TelegramBot;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -9,8 +10,8 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+@Slf4j
 @Component
-
 public class BotInitializer {
     @Autowired
     TelegramBot bot;
@@ -20,7 +21,7 @@ public class BotInitializer {
         try {
             telegramBotsApi.registerBot(bot);
         } catch (TelegramApiException e) {
-
+            log.error("Error occurred: " + e.getMessage());
         }
     }
 }
